8694650b67b84503845c0341a47bd7332f5aa96d
KubaM <jakub.m.makowiecki@gmail.com>
2024-02-26 22:24:17 +0100

    implemented AppConfig class

--- src/main/java/com/app/cars/service/impl/UsersServiceImpl.java
+++ src/main/java/com/app/cars/service/impl/UsersServiceImpl.java
@@ -1,11 +1,43 @@
 package com.app.cars.service.impl;
 
+import com.app.cars.persistence.repository.UserEntityRepository;
 import com.app.cars.service.UsersService;
 import com.app.cars.service.dto.CreateUserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@Service
+@RequiredArgsConstructor
 public class UsersServiceImpl implements UsersService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserEntityRepository userEntityRepository;
+
     @Override
     public Long registerUser(CreateUserDto createUserDto) {
-        return null;
+        // TODO do zaimplementowania warstwa walidacji
+        // na razie zostawiam trzy ważne warunki poniżej
+
+        if (!createUserDto.password().equals(createUserDto.passwordConfirmation())) {
+            throw new IllegalArgumentException("Passwords are not correct");
+        }
+
+        if (userEntityRepository.findByUsername(createUserDto.username()).isPresent()) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        if(userEntityRepository.findByEmail(createUserDto.email()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+
+        var userToInsert = createUserDto
+                .toUserEntity()
+                .withPassword(passwordEncoder.encode(createUserDto.password()));
+
+        var insertedUser = userEntityRepository.save(userToInsert);
+
+        return insertedUser.getId();
     }
 }
