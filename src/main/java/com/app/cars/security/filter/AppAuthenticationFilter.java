a67ed4bd4d1b6d2b50985d85bf2a1e2dc70c7c7d
KubaM <jakub.m.makowiecki@gmail.com>
2024-04-29 23:45:19 +0200

    implemented attemptAuthentication method

--- src/main/java/com/app/cars/security/filter/AppAuthenticationFilter.java
+++ src/main/java/com/app/cars/security/filter/AppAuthenticationFilter.java
@@ -1,19 +1,48 @@
 package com.app.cars.security.filter;
 
+import com.app.cars.security.dto.AuthenticationDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
+// domyslnie ten filter wywola metode attemptAuthentification kiedy na rzecz aplikacji wywola sie
+// request POST -> /login
+// domyslne URI mozna zmienic w konstruktorze klasy
 public class AppAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+
+    public AppAuthenticationFilter(TokenService tokenService, AuthenticationManager authenticationManager) {
+        this.tokenService = tokenService;
+        this.authenticationManager = authenticationManager;
+        //setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/login", "POST")); -> przykładowa zmiana URI
+
+    }
+
+    @SneakyThrows
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        var jsonBody = new ObjectMapper()
+                .readValue(request.getInputStream(), AuthenticationDto.class);
+
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                jsonBody.username(),
+                jsonBody.password(),
+                Collections.emptyList()
+        ));
     }
 
     @Override
